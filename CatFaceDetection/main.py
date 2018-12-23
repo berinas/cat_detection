5478ae53843a11aa07e342f2f515827985bb6f44
berinas <bsmajovic1@etf.unsa.ba>
2018-12-23 23:17:41 +0100

    mala popravka

--- CatFaceDetection/main.py
+++ CatFaceDetection/main.py
@@ -5,9 +5,11 @@ import cv2
 from poboljsavanje_slika import *
 from create_descriptor import *
 from model_training import *
-import pickle
+
 
 path = 'C:/Users/Berina/Desktop/cat_detection/slike_finalnaValidacija'
+loaded_model = joblib.load(open('finalized_model.sav', 'rb'))  # model
+
 def main(path):
     br = 0
     valid_images = [".jpg", ".jpeg"]
@@ -17,9 +19,10 @@ def main(path):
         if ext.lower() not in valid_images:
             continue
         image = Image.open(os.path.join(path, f))
-        image = np.array(image)
         new_image = poboljsaj(image)    # poboljsavanje slika-ujednacavanje histograma+osvjetljenje
         hf, h_image = createDescriptor(image)  # kreiranje deskriptora (HOG)
-        loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # model
 
         br += 1
+
+
+main(path)
